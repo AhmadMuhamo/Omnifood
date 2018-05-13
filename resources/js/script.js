1867157ebf2608bd855a54c60473f222c15bdb38
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2018-05-13 20:27:43 +0200

    Responsive Navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -70,4 +70,19 @@ $(document).ready(function () {
     }, {
         offset: '70%'
     });
+
+    /* Navbar Collapse */
+    $('#nav-collapse').click(function() {
+        var nav = $('#main-nav');
+        var icon = $('.nav-collapse i');
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
